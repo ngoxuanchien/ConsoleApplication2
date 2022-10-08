48001861dbfcc889cf084a4e3ca9479c25ec5e94
ngoxuanchien <ngoxuanchien9a@gmail.com>
2022-10-08 22:14:59 +0700

    1

--- FAT32.h
+++ FAT32.h
@@ -5,41 +5,32 @@
 class FAT32
 {
 private:
-	ll _sizeSector;
-	ll _sizeCluster;
-	ll _sizeFat;
-	ll _sizeVolume;
-	int _numFat;
-	ll _sizeBootSector;
-	string _typeFat;
+	string _jmpInstruction;
+	string _OEMID;
+	int _bytesPerSector;
+	int _sectorsPerCluster;
+	int _reservedSector;
+	int _numberOfFATs;
+	string _mediaDescriptor;
+	int _sectorPerTrack;
+	int _numberOfHeads;
+	int _hiddenSectors;
+	ll _totalSector;
+	ll _sectorPerFAT;
+	string _version;
+	int _rootCluster;
+	int _systemInformation;
+	int _backupBootSector;
+	string _physicalDrive;
+	string _extendedSignature;
+	string _serialNumber;
+	string _volumeNumber;
+	string _fileSystem;
+	BYTE _bootstrapCode[420];
+	string _signature;
+	
 	vector<File*> _listFile;
 
 public:
-	FAT32();
-	FAT32(LPCWSTR);
-	~FAT32();
-
-	// getter
-	void setSizeSector(const ll&);
-	void setSizeCluster(const ll&);
-	void setSizeFat(const ll&);
-	void setSizeVolume(const ll&);
-	void setListFile(const vector<File*>);
-	void setNumFat(const int&);
-	void setSizeBootSector(const ll&);
-	void setTypeFat(const string&);
-
-	// sector
-	ll getSizeSector();
-	ll getSizeCluster();
-	ll getSizeFat();
-	ll getSizeVolume();
-	vector<File*> getListFile();
-	ll getRDETLocation();
-	ll getFATLocation();
-	int getNumFat();
-	ll getSizeBootSector();
-	string getTypeFat();
-
 };
 
